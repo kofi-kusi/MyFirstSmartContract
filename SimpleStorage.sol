3a3f465b311774d83bd46d89228f0bee92a8abae
kofi-kusi <appaukusi05@gmail.com>
2024-10-24 13:24:25 +0000

    added variables and functions

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,4 +3,46 @@ pragma solidity ^0.8.25;
 
 contract SimpleStorage {
     uint256 public favoriteNumber;
+    uint256[] public favoriteNumbers; // Dynamic array to store multiple favorite numbers
+
+    // Struct to store a person’s name and their favorite number
+    struct Person {
+        string name; // Person's name
+        uint256 favoriteNumber; // Their favorite number
+    }
+
+    Person[] public people; // Array of Person structs to store multiple people's data
+
+    // Enum to manage contract states (Active or Inactive)
+    enum ContractState {
+        Active, // State representing an active contract
+        Inactive // State representing an inactive contract
+    }
+
+    ContractState public state; // State variable to store the current contract state
+
+    function storeNumber(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function getFavoriteNumber() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function isGreaterThan(uint256 _compareValue) public view returns(bool) {
+        if (favoriteNumber > _compareValue) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function sumToFavoriteNumber() public view returns (uint256) {
+        uint256 sum = 0;
+        for (uint256 i = 1; i <= favoriteNumber; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 }
+
